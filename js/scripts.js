d6aa37ce77efe0349154e45ef0cda30171945d9f
Joy Vivika <joiemumo@gmail.com>
2018-09-28 17:49:51 +0300

    add cards and container

---
+++ js/scripts.js
@@ -0,0 +1,55 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $(document).ready(function() {
+//     $("#status").click(function() {
+//     function rollDice() {
+//     var d1=Math.ceil(Math.random()* 6)+ 1;
+//     var d2=Math.ceil(Math.random()* 6)+ 1;
+     
+//     var diceTotal = function(d1,d2){
+//         return (d1+ d2 );
+//         }
+        
+//         // var result = testResults(q1, q2 , q3,);
+        
+        
+//         //   document.getElementById("display").style.visibility="visible";
+//         //   document.getElementById("answer").innerHTML=" You got " +  result + " result. ";
+        
+        
+//         // }
+
+// // }
+// // var die1;
+// // die1=Math.ceil(Math.random()*6);
+// // document.textContent(die1);
